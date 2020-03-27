474e458b303834f1eaf64e250df58df517f173af
Leonardo Gallegos <irochivas@gmail.com>
2020-03-27 00:02:45 -0600

    VersionPHP

--- Servidor PHP MySQL/server/index.php
+++ Servidor PHP MySQL/server/index.php
@@ -1,31 +1,4 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Alumnos index</title>
-  </head>
-  <style type="text/css">
-	table, th, td{
-		border: 1px solid black;
-		border-collapse: collapse;
-	}
-	th, td{
-		padding:  10px;
-	}
-	th{
-		background: #3498DB;
-		font:bold;
-	}
-	td{
-		background: #EBF5FB;
-	}
-</style>
 
-  <body bgcolor = #17202A>
-    <font color="#fff0ff">
-    <CENTER><h1>Holi</h1>
-    </font>
-   <img src = "https://i.pinimg.com/originals/38/bc/4d/38bc4d97808f849f0bc1ed98c2b83996.jpg">
-   </CENTER>
-  </body>
-</html>
+<?php
+	phpinfo();
+?>
